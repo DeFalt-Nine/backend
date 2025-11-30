1b5649aaea4447362a3689c097c37776e20d4f39
unknown <alforick8@gmail.com>
2025-11-30 16:01:39 +0800

    some updated files

--- models/LocalEvent.js
+++ models/LocalEvent.js
@@ -4,10 +4,10 @@ const mongoose = require('mongoose');
 const LocalEventSchema = new mongoose.Schema({
   image: { type: String, required: true },
   title: { type: String, required: true },
-  date: { type: String, required: true },
+  date: { type: String, required: false },
   description: { type: String, required: true },
   location: { type: String, required: true },
-  badge: { type: String, required: true },
+  badge: { type: String, required: false },
 });
 
 module.exports = mongoose.models.LocalEvent || mongoose.model('LocalEvent', LocalEventSchema, 'localevents');
